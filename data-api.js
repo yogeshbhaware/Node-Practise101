8b9a430433e14a437071eb970e35c05a4594d88d
yogesh <yogesh@rapidinnovation.dev>
2023-11-02 20:08:41 +0530

    "Inital Commit"

---
+++ data-api.js
@@ -0,0 +1,7 @@
+const data = [
+  { name: "Yogesh Kumar Bhaware", email: "yogesh@gmail.com" },
+  { name: "Amitabh Bacchan", email: "amit@gmail.com" },
+
+  { name: "Salman Khan", email: "salaman@gmail.com" },
+];
+module.exports = data;
---
+++ index.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const data = require('./data-api');
+
+http.createServer((req, resp)=>{
+    resp.writeHead(200, {'content-Type':'application\json'});
+    resp.write(JSON.stringify(data))
+    resp.end();
+}).listen(4500)
